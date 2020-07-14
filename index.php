a35eb99095a44a0aafc9f4afc9607e7c57b3951b
chumex412 <64347090+chumex412@users.noreply.github.com>
2020-07-14 17:43:45 +0100

    JElite

--- index.php
+++ index.php
@@ -11,9 +11,15 @@
 
          <meta name="search" name="">
 
+         <!-- Google Font -->
+         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+
 		 <!---  Bootstrap CSS -->
 		 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
 		 	integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        
+        <!--- Custom CSS -->
+        <link rel="stylesheet" href="assets/css/Custom css/style.css">
 
 
          <title></title>
@@ -23,7 +29,44 @@
 
         <section id="Hero">
             <header>
+                <div class="container-fluid p-5">
+                    <div class="hero-header">
+                        <img class="img-fluid ml-3" src="assets/images/header-image.png" alt="Barista">
+                        <h3 class="head-name">Barista</h3>
+                    </div>
 
+                    <div class="hero-content">
+                        <div class="row">
+                            <div class="col-md-5 order-md-1 order-lg-1 order-2">
+                                <h3>COMING SOON</h3>
+                                <strong>The smart Barista</strong>
+
+                                <p>
+                                    This app detects your need for some caffeine and alerts your coffee 
+                                    machine to make coffee for you. It always gets the coffee machine to get 
+                                    your coffee ready at the exact time you always need your coffee.
+                                </p>
+                                <p>
+                                    Stay Connected and be notified of the app launching
+                                </p>
+
+                                <form class="form-inline mt-5" action="Admin/action.php" method="POST">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" name="email_subs" placeholder="Enter Your Email Address"  maxlength="30" required/>
+                                    </div>
+                                    <input type="submit" name="submit" value="Notify Me" class="btn btn-secondary">
+                                </form>
+                            </div>
+
+                            <div class="col-md-7 order-md-2 order-lg-2 order-1">
+                                <img class="img-fluid" src="assets/images/hero-image.png">
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
             </header>
         </section>
 
@@ -36,14 +79,7 @@
         </section>
 
         <section id="moreInfo">
-            <div class="container">
-                <form class="form-inline mt-5" action="Admin/action.php" method="POST">
-                    <div class="form-group">
-                        <input type="email" class="form-control" name="email_subs" placeholder="Enter email"  maxlength="30" required/>
-                    </div>
-                    <input type="submit" name="submit" value="Submit" class="btn btn-warning">
-                </form>
-            </div>
+            
         </section>
 
 
